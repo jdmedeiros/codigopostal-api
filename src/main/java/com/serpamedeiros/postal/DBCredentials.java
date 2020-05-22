ce28fa3861277c5770fea07ca97e5da5b3cf910b
Medeiros <medeiros@serpamedeiros.com>
2020-05-22 13:48:18 +0000

    Added separate credentials file.

---
+++ src/main/java/com/serpamedeiros/postal/DBCredentials.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.serpamedeiros.postal;
+
+/**
+ *
+ * @author enta
+ */
+public class DBCredentials {
+
+    public static String userid = "root";
+    public static String password = "changeme";
+    public static String databaseUrl = "jdbc:mysql://ctt.cmwy6qjl36fn.us-east-1.rds.amazonaws.com/ctt";
+}
